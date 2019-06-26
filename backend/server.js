f1fb4c8e32d70caf8b4ba10f5196cb3cc7b99a5b
Rere <renitadewi55@gmail.com>
2019-06-26 21:44:58 +0700

    comment

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!')) //req : request, res : respons
+//GET KHUSUS MENGAMBIL DATA2 YANG ADA DI DATABASE
+app.get('/', (req, res) => res.send('Hello NodeJS!')) //req : request, res : respons
+
 app.listen(3000, () => console.log('Example app listening on port 3000!')) //listen mengaktifkan servernya di port 3000
