8e4f3bd8e885aa888ecac30a2ab263e142cd0370
Rere <renitadewi55@gmail.com>
2019-06-29 02:14:14 +0700

    Create and update controllers

---
+++ backend/controllers/TaskDeleteController.js
@@ -0,0 +1,3 @@
+module.exports = (req, res) => {
+    res.send('Hello NodeJS!') //req : request, res : respons
+}
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,18 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
+const TaskListController = require('./controllers/TaskListController.js')
+const TaskCreateController = require('./controllers/TaskCreateController.js')
+const TaskDeleteController = require('./controllers/TaskDeleteController.js')
+const TaskUpdateController = require('./controllers/TaskUpdateController.js')
 
 //GET KHUSUS MENGAMBIL DATA2 YANG ADA DI DATABASE
-app.get('/', (req, res) => res.send('Hello NodeJS!')) //req : request, res : respons
+app.get('/', TaskListController)
+//untuk menambahkan data
+app.post('/', TaskCreateController)
+//untuk menghapus data
+app.delete('/', TaskDeleteController)
+//untuk mengupdatedata
+app.put('/', TaskUpdateController)
 
 app.listen(3000, () => console.log('Example app listening on port 3000!')) //listen mengaktifkan servernya di port 3000
