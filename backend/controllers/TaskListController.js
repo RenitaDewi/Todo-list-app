d857436e0b4b1f7c4a345c6902a0d1575807db20
Rere <renitadewi55@gmail.com>
2019-06-30 22:02:04 +0700

    Menambahkan objek di db

--- backend/controllers/TaskListController.js
+++ backend/controllers/TaskListController.js
@@ -1,3 +1,5 @@
+const mysql = require('../database')
 module.exports = (req, res) => {
+    mysql.query()
     res.send('Hello NodeJS!') //req : request, res : respons
 }
--- backend/server.js
+++ backend/server.js
@@ -6,13 +6,14 @@ const TaskCreateController = require('./controllers/TaskCreateController.js')
 const TaskDeleteController = require('./controllers/TaskDeleteController.js')
 const TaskUpdateController = require('./controllers/TaskUpdateController.js')
 
+//REST: HTTP verbs
 //GET KHUSUS MENGAMBIL DATA2 YANG ADA DI DATABASE
 app.get('/', TaskListController)
 //untuk menambahkan data
 app.post('/', TaskCreateController)
 //untuk menghapus data
-app.delete('/', TaskDeleteController)
+app.delete('/:id', TaskDeleteController)
 //untuk mengupdatedata
-app.put('/', TaskUpdateController)
+app.put('/:id', TaskUpdateController)
 
 app.listen(3000, () => console.log('Example app listening on port 3000!')) //listen mengaktifkan servernya di port 3000
